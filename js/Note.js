470ce180ff01f91e416f1c4e00ffd0bdbc420954
seunzone <darealseun@gmail.com>
2017-07-24 11:44:15 +0100

    interface

--- js/Note.js
+++ js/Note.js
@@ -1,6 +1,23 @@
 var Note = React.createClass({
+    edit: function() {
+        alert('editing note');
+    },
+    remove: function() {
+        alert('removing note');
+    },
     render: function() {
-        
+        return (
+            <div className="note">
+                <p>{this.props.children}</p>
+                <span>
+                    <button onClick={this.edit}
+                            className="btn btn-primary glyphicon glyphicon-pencil"/>
+                    <button onClick={this.remove}
+                            className="btn btn-danger glyphicon glyphicon-trash"/>
+                </span>
+            </div>
+            );
+    }
 });
 
 React.render(<Note>Hello World</Note>, 
