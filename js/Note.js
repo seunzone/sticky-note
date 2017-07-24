3c1a0133566f78c1ea770809d10ec6c11198fcf7
seunzone <darealseun@gmail.com>
2017-07-24 13:23:16 +0100

    Interface Adding up

--- js/Note.js
+++ js/Note.js
@@ -1,11 +1,17 @@
 var Note = React.createClass({
+    getInitialState: function() {
+        return {editing: false}
+    },
     edit: function() {
-        alert('editing note');
+        this.setState({editing: true});
+    },
+    save: function() {
+        this.setState({editing: false});
     },
     remove: function() {
         alert('removing note');
     },
-    render: function() {
+    renderDisplay: function() {
         return (
             <div className="note">
                 <p>{this.props.children}</p>
@@ -17,6 +23,23 @@ var Note = React.createClass({
                 </span>
             </div>
             );
+    },
+    renderForm: function() {
+        return (
+            <div className="note">
+            <textarea defaultValue={this.props.children} 
+            className="form-control"></textarea>
+            <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
+            </div>
+            )
+    },
+    render: function() {
+        if (this.state.editing) {
+            return this.renderForm();
+        }
+        else {
+            return this.renderDisplay();
+        }
     }
 });
 
