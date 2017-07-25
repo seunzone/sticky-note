32a75f18bb9ace40d4cb5d1a3a2488f3ece618c9
seunzone <darealseun@gmail.com>
2017-07-25 14:00:00 +0100

    The Board Component

--- js/Note.js
+++ js/Note.js
@@ -45,5 +45,22 @@ var Note = React.createClass({
     }
 });
 
-React.render(<Note>Hello World</Note>, 
+var Board = React.createClass({
+    propTypes:{
+        count:function(props, propName){
+            if(typeof props[propName] !== "number"){
+                return new Error ('Count must be a number');
+            } 
+            if (props[propName] > 100){
+                return new Error("this your" + props[propName] + " should be reduced");
+            }
+        }
+    },
+    render: function(){
+        return <div className="board">{this.props.count}
+        </div>
+    }
+});
+
+React.render(<Board count={10} />, 
     document.getElementById('react-container'));
